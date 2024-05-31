269fc5e4ed5fdfa5f81ae24a06477720fd80e6f7
Ganesh Regmi <98951707+regmiganesh@users.noreply.github.com>
2024-05-31 17:46:23 +0545

    Add files via upload

---
+++ day2/app.js
@@ -0,0 +1,88 @@
+let x = 10;
+let y = 2;
+let z = x + y;
+
+console.log(z);
+
+let a = 10;
+let b = 20;
+let c = a - b;
+console.log(c);
+
+let d = 20;
+let e = 10;
+let f = d * e;
+console.log(f);
+
+// Arithmetic Operators Example
+
+let g = 3;
+let h = 10 * 30 * g;
+console.log(h);
+
+let news = 10;
+news += 2;
+console.log(news);
+
+// JavaScript String Comparison
+
+let text1 = "ganesh regmi";
+let text2 = "ganesh";
+let result = text1 == text2;
+console.log(result);
+
+let num1 = 5;
+let num2 = 10;
+let num3 = num2 > num1;
+console.log(num3);
+
+let text4 = "Ganesh";
+let text5 = "Regmi";
+
+let finalText = text4 + "" + text5;
+console.log(finalText);
+
+// The += assignment operator can also be used to add (concatenate) strings:
+
+let textone = "What a very";
+textone += "nice day";
+console.log(textone);
+
+// Adding two numbers, will return the sum, but adding a number and a string will return a string:
+
+let num = 5 + 5;
+let numm = "5" + 5;
+let ans = "Hello" + 5;
+console.log(numm);
+console.log(ans);
+
+// JS DATA TYPE
+
+//number
+let length = 16;
+let weight = 7.5;
+
+//string
+let color = "green";
+let LastName = "Regmi";
+
+//boolean
+let first = true;
+let second = false;
+
+//object
+
+const person = {
+  Name: "ganesh",
+  age: 24,
+  address: "Lamki kailali",
+};
+
+//array object
+
+const fruits = ["apple", "banana", "pineapple", "orange"];
+
+//date object
+
+const date = new Date();
+console.log(date);
