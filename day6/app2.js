786ba191500ef2a7d86fb81d089f1301ad22fb5a
Ganesh Regmi <98951707+regmiganesh@users.noreply.github.com>
2024-06-05 19:39:49 +0545

    Add files via upload

---
+++ day6/app2.js
@@ -0,0 +1,12 @@
+// JavaScript Template Strings
+
+// Template Strings use back-ticks (``) rather than the quotes ("") to define a string:
+
+let text = `Hello Word!`;
+console.log(text);
+
+let firstName = "Ganesh";
+let lastName = "Regmi";
+
+let Final = `hello i am ${firstName} ${lastName}`;
+console.log(Final);
