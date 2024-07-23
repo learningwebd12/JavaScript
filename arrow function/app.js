ebd031255d7e9f072b78bc7260a9a2fa97b2c404
Ganesh Regmi <98951707+regmiganesh@users.noreply.github.com>
2024-07-23 12:47:01 +0545

    Add files via upload

---
+++ arrow function/app.js
@@ -0,0 +1,28 @@
+function Helo() {
+  console.log("Ganesh");
+}
+
+Helo();
+
+let Abc = function () {
+  console.log("Nepal");
+};
+
+Abc();
+
+let Hello = () => {
+  console.log("Hey");
+};
+Hello();
+
+var Add = (a, b) => {
+  return a + b;
+};
+console.log(Add(10, 10));
+
+const Nep = () => {
+  for (i = 0; i < 5; i++) {
+    console.log("Hey");
+  }
+};
+Nep();
