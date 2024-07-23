8dbc318f03cf5dca7f1fd38bc651b9109c2e1969
Ganesh Regmi <98951707+regmiganesh@users.noreply.github.com>
2024-07-23 12:24:30 +0545

    Add files via upload

---
+++ let,Var,Const/app.js
@@ -0,0 +1,37 @@
+var x = "Hello";
+var x = "wow";
+x = "Hey";
+console.log(x);
+
+let y = "Name";
+// let y = "LastName"; //declare garna mildai na
+y = "Wow"; // value assign garna milxa
+console.log(y);
+
+const N = "Nepal";
+// const N = "India";
+// N = "china";
+
+console.log(N);
+
+// var Global Scope
+if (2 == 2) {
+  var mm = "Nepal is Beautiful Country";
+}
+console.log(mm);
+
+//Let Block Scope
+
+if (2 == 2) {
+  let nn = "Nepal is Beautiful Country";
+  console.log(nn);
+}
+// console.log(nn);
+
+//Const block scope
+
+if (2 == 2) {
+  let z = "Nepal is beautyful country";
+  console.log(z);
+}
+// console.log(z);
